c75266b56a5ae410fdf3d74146944d43588925b7
Dhvani Gupta <63143690+dhvani-gupta@users.noreply.github.com>
2022-09-29 15:39:47 +0530

    Add files via upload
    Max of 3 numbers

---
+++ exp3.sh
@@ -0,0 +1,13 @@
+echo "Enter three number:"
+read num1 
+read num2
+read num3
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then
+    echo $num1
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then
+    echo $num2
+else
+    echo $num3
+fi
