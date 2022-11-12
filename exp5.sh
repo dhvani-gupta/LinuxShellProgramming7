05158f5ed63485e675ba48c352440ca99cae8d2f
Dhvani Gupta <63143690+dhvani-gupta@users.noreply.github.com>
2022-11-12 13:02:49 +0530

    Add files via upload

---
+++ exp5.sh
@@ -0,0 +1,13 @@
+echo "Enter number:"
+read n
+
+for((i=2; i<=$n/2; i++))
+do
+  ans=$(( n%i ))
+  if [ $ans -eq 0 ]
+  then
+    echo "$n is not a prime number."
+    exit 0
+  fi
+done
+echo "$n is a prime number."
